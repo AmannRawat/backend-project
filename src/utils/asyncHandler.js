b25568a7e7fd98cd5f74d2da836eaaf32249ff88
AmannRawat <amanrawat1509@gmail.com>
2025-12-22 13:58:10 +0530

    Utility files Created

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asynchandler=(requestHandler)=>async(req,res,next)=>{
+    try{
+        await requestHandler(req,res,next);
+    }   catch(err){
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+
+export default asynchandler;
