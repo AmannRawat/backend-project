fc15865a0b7420b796a0069f50124bd56a26e0f6
AmannRawat <amanrawat1509@gmail.com>
2025-12-23 12:18:44 +0530

    Encryption by bcrypt established

--- src/models/user.mode.js
+++ src/models/user.mode.js
@@ -1,51 +1,91 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
     {
-        userName:{
+        userName: {
             type: String,
             required: true,
-            unique : true,
-            lowercase : true,
+            unique: true,
+            lowercase: true,
             trim: true,
-            index:true,
+            index: true,
         },
-        email :{
+        email: {
             type: String,
             required: true,
-            unique : true,
-            lowercase : true,
+            unique: true,
+            lowercase: true,
             trim: true,
         },
-        fullName :{
+        fullName: {
             type: String,
             required: true,
             trim: true,
         },
-        avatar :{
+        avatar: {
             type: String,   //Cloudinary URL
             required: true,
         },
-        coverImage :{
+        coverImage: {
             type: String,   //Cloudinary URL
         },
-        watchHistory :[
+        watchHistory: [
             {
                 type: Schema.Types.ObjectId,
                 ref: "Video",
             }
         ],
-        password :{
+        password: {
             type: String,
-            required: [true,'Password is required'],
+            required: [true, 'Password is required'],
         },
-        refreshToken :{
+        refreshToken: {
             type: String,
         },
-    },{
-        timestamps: true,
-    }
+    }, {
+    timestamps: true,
+}
 );
 
+//Middleware to hash password before saving user
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
