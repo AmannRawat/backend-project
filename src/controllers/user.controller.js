ccac97d2c9cf26754f4e021d713e36db0ad4558a
AmannRawat <amanrawat1509@gmail.com>
2025-12-25 21:14:01 +0530

    Implementing Logic of user registeration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import asynchandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
-/* Steps to Register User
+/* ALGORITHM
+    Steps to Register User
     1- Get User Details from frontend
     2- Validation (if data is correct eg: Not Empty)
     3- Check if user alredy Exist (email,username which are unique)
@@ -10,8 +13,35 @@ import asynchandler from "../utils/asyncHandler.js";
     7- Remove password and refresh token field from response
     8- Check for user creation
     9- Return response or error if failed
-*/ 
-const registerUser = asynchandler(
+*/
+const registerUser = asynchandler(async (req, res) => {
+    // getting User data
+    const { fullName, email, username, password } = req.body
+    console.log("NAME:", fullName);
+
+    // This Validation part is tough to understand
+    if (
+        [fullName, username, email, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All Fields are Required")
+    }
+
+    // Check user exists or not
+    const existedUser= User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User already exists with given username or email")
+    }
+
+    // Check for images
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+}
 );
 
 export default registerUser;
